9fab72443a5c38503fd63e090deeeaaf617620df
digitalunknown <piotr.osmenda@gmail.com>
2025-12-16 16:38:31 -0600

    Initial Commit

---
+++ Trip PlannerTests/Trip_PlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Trip_PlannerTests.swift
+//  Trip PlannerTests
+//
+//  Created by Piotr Osmenda on 12/16/25.
+//
+
+import Testing
+@testable import Trip_Planner
+
+struct Trip_PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
